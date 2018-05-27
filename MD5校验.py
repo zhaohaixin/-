123b73d5dc69307999ca1ca4d5a695d9e4e7e204
zhaohaixin <fhzhaohaixin@126.com>
2018-05-27 15:38:25 +0800

    增加MD5校验的GUI界面

--- MD5校验.py
+++ MD5校验.py
@@ -1,22 +1,20 @@
 import hashlib
 import os
+from tkinter import *
+import tkinter.messagebox as messagebox
 
-
-######大文件MD5校验##########
 def get_md5_02(file_path):
-    f = open(file_path, 'rb')
-    md5_obj = hashlib.md5()
-    while True:
-        d = f.read(8096)
-        if not d:
-            break
-        md5_obj.update(d)
-    hash_code = md5_obj.hexdigest()
-    f.close()
-    md5 = str(hash_code).lower()
-    return md5
-
-
+        f = open(file_path, 'rb')
+        md5_obj = hashlib.md5()
+        while True:
+            d = f.read(8096)
+            if not d:
+                break
+            md5_obj.update(d)
+        hash_code = md5_obj.hexdigest()
+        f.close()
+        md5 = str(hash_code).lower()
+        return md5
 def get_md5_01(file_path):
     md5 = None
     if os.path.isfile(file_path):
@@ -27,22 +25,34 @@ def get_md5_01(file_path):
         f.close()
         md5 = str(hash_code).lower()
     return md5
-
-
-rootdir = '/Users/zhaohaixin/Desktop/微信监控'
 # 对路径下文件夹内的所有文件做md5值计算,并在该目录下生成md5计算文件
 
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-def mdf_cal(rootdir):
-    md5_file = open(os.path.join(rootdir, 'md5_file.txt'), 'w')
-    # md5_file.truncate()
-    list = os.listdir(rootdir)  # 列出文件夹下所有的目录与文件
-    for i in range(0, len(list)):
-        path = os.path.join(rootdir, list[i])
-        if os.path.isfile(path):
-            # 你想对文件的操作
-            md5_file.write(os.path.basename(path)+': '+get_md5_01(path)+'\n')
-    md5_file.close()
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='计算MD5', command=self.md5_cal)
+        self.alertButton.pack()
+        ######大文件MD5校验##########
+    
+    def md5_cal(self):
+        # md5_file.truncate()
+        list = os.listdir(self.nameInput.get())  # 列出文件夹下所有的目录与文件
+        md5_file = open(os.path.join(self.nameInput.get(), 'md5_file.txt'), 'w')
+        for i in range(0, len(list)):
+            path = os.path.join(self.nameInput.get(), list[i])
+            if os.path.isfile(path):
+                # 你想对文件的操作
+                md5_file.write(os.path.basename(path)+': '+get_md5_01(path)+'\n')
+        md5_file.close()
 
-
-mdf_cal(rootdir)
+app = Application()
+# 设置窗口标题:
+app.master.title('MD5校验')
+# 主消息循环:
+app.mainloop()
